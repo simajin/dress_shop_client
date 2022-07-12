a3130cced0287300c14502137b2d13b7f691d796
sajj <tututu123@naver.com>
2022-07-12 17:47:23 +0900

    메인화면 완성

--- src/include/Footer.js
+++ src/include/Footer.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
 const Footer = () => {
+    let info = 1;
+    function showFooter(){
+        const footerMenu = document.querySelector("#hideFooter");
+        const footerBtn = document.querySelector("#footerBtn");
+
+        if(info === 1){
+            footerMenu.style.bottom ="0";
+            footerMenu.style.transition = "0.5s";
+            setTimeout(()=>{
+                footerBtn.style.color = "#666";
+            },200)
+            info = 0 ;
+        }else if(info === 0){
+            footerMenu.style.bottom ="-150px";
+            footerMenu.style.transition = "0.5s";
+            setTimeout(()=>{
+                footerBtn.style.color = "#fff";
+            },200)
+            info = 1;
+        }
+        return info;
+
+    }
     return (
         <div id="footer">
-            <div className='inner'>
-                
+            <button id="footerBtn" onClick={showFooter}>ⓘ</button>
+            <div id='hideFooter'>
+                <div id='footerInner'>
+                    <p>2022 © W Wedding. All Rights Reserved.</p>
+                    <p>법인명 : (주)더블유 웨딩 │ 사업자등록번호: 111-23-445678</p>
+                    <p>대표번호 : <span>052-123-0000</span> │주소: 울산광역시 남구 삼산로 1│ 대표자명: 김예진</p>
+                </div>
+                <h1 id='logoBot'><svg id="wLogoBot" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 50"><path id="logoLineBot" className="cls-2" d="M28.36,26S8.5,30.5,5.5,19.5s29-18,30-4a243.83,243.83,0,0,1,0,29h1.65S49.53,21.76,49.52,24.63,52.29,43.5,60.4,44.5s-1.9-11,14.1-40"/></svg></h1>
             </div>
         </div>
     );
--- src/include/Header.js
+++ src/include/Header.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 
 const Header = () => {
-    let menuBar = document.querySelector('#menu_bar');
-    // function menuClick() {
-    //     // menuBar.addEventListener('click',()=>{
-    //     let menu = document.querySelector('#menu');
-    //     menu.style.right = '50px';
-    //     menu.style.transition = '0.5s';
-    //     // })
-    // }
+    // 메뉴 클릭이벤트 한번 눌렀을때
+    function menuClick() {
+        let menu = document.querySelector('#menu');
+        let bugerTab = document.querySelectorAll('.bugerTab');
+        let menuBar = document.querySelector('#menu_bar');
+        menu.style.right = '0';
+        menu.style.transition = '0.5s';
+        bugerTab[0].style.display = 'none';
+        bugerTab[1].style.display = 'none';
+        // 메뉴 나왔을때 새로운 X버튼 생성
+    }
+    function menuClose(){
+        let menu = document.querySelector('#menu');
+        let bugerTab = document.querySelectorAll('.bugerTab');
+        menu.style.right = '-650px';
+        menu.style.transition = '0.5s';
+        setTimeout(() => {
+            bugerTab[0].style.display = 'block';
+            bugerTab[1].style.display = 'block';
+        }, 300);
+
+    }
+    
+
+    // 로고 이벤트
     let isEventOn = true;
     function logoEvent(){
         const logo = document.getElementById('wLogo');
@@ -31,15 +48,16 @@ const Header = () => {
         <div id="header">
             <div className='inner'>
                 <h1><svg id="wLogo" onMouseEnter = {logoEvent} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 50"><path id="logoLine" className="cls-1" d="M28.36,26S8.5,30.5,5.5,19.5s29-18,30-4a243.83,243.83,0,0,1,0,29h1.65S49.53,21.76,49.52,24.63,52.29,43.5,60.4,44.5s-1.9-11,14.1-40"/></svg></h1>
-                <div id="menu_bar" >
-                    <span></span>
-                    <span></span>
+                <div id="menu_bar" onClick={menuClick}>
+                    <span className='bugerTab'></span>
+                    <span className='bugerTab'></span>
                 </div>
                 <ul id="menu">
                     <li>ABOUT</li>
                     <li>SHOP</li>
                     <li>CART</li>
                     <li>SEARCH</li>
+                    <li><button className='barBtn' onClick={menuClose}>X</button></li>
                 </ul>
             </div>
         </div>
