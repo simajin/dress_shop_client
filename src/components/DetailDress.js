f726b0f45e5627524e2e6ac00b34d5fde2a4e9a6
sajj <tututu123@naver.com>
2022-07-22 15:36:03 +0900

    카트완료

--- src/components/DetailDress.js
+++ src/components/DetailDress.js
@@ -3,20 +3,22 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import './DetailDress.css'
 import { useNavigate } from 'react-router-dom';
+import Aside from '../include/Aside';
 
 const DetailDress = () => {
     const navigate = useNavigate();
-    // const slt = document.querySelector("#slt");
+    const slt = document.querySelector("#slt");
     // const opQtt = document.querySelectorAll(".qtt");
-    // const qttNum = document.querySelector("#qttNum");
+    const qttNum = document.querySelector("#qttNum");
     const [ dress, setDress ] = useState(null);
     const { id } = useParams();             // id값 받아오기
     //상태관리 - 이미지 변경
     const detailImg = document.querySelectorAll(".detailImg");
     const [ mainImg, setMainImg ] = useState('');
-    const [ qtt, setQtt] = useState('')
+    const [ maxQtt, setMaxQtt] = useState('');
+    const [ qtt , setQtt ] = useState('');
     const [ currentImg, setCurrentImg ] = useState(0);
-    
+    const [ cartData, setCartData ] = useState();
     useEffect(()=>{
         axios.get(`http://localhost:8000/dress/${id}`)
         .then(result => {
@@ -24,7 +26,15 @@ const DetailDress = () => {
             const resulta = result.data;
             setDress(resulta[0]); 
             setMainImg('http://localhost:3000/'+resulta[0].imgsrc);
-            setQtt(resulta[0].size1); 
+            setMaxQtt(resulta[0].size1); 
+            setCartData({
+                    c_img : resulta[0].imgsrc,
+                    c_name : resulta[0].name,
+                    c_price : resulta[0].price,
+                    c_size : "",
+                    c_amount : qtt,
+                    c_userid : "",
+            });
         })   
         .catch(e=> {
             console.log(e);
@@ -40,7 +50,6 @@ const DetailDress = () => {
             setCurrentImg((current)=>current+1)
         }
     }, 4000);
-
     //상품 삭제
     const onDelete = () => {
         axios.delete(`http://localhost:8000/delDress/${id}`)
@@ -50,6 +59,57 @@ const DetailDress = () => {
         })
         .catch(err=>{
             console.log(err);
+        })
+    }
+    const amountChange = ()=>{
+        setCartData(
+            ...cartData,
+        )
+    }
+    //카트 추가
+    const addToCart = ()=>{
+        axios.post(`http://localhost:8000/addToCart`, cartData)
+        .then(res=>{
+            console.log("카트추가완료");
+            if( window.confirm("장바구니에 담겼습니다. 장바구니로 가시겠습니까?")){
+                navigate('/cart');
+                console.log("옙")
+            }else{
+                console.log("놉")
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+    const selectOnChange = (e) => {
+        const { value } = e.target
+        let num = "S"
+        console.log('여기에요')
+       
+        if(value==="1"){
+            setMaxQtt(dress.size1)
+            num = "S"
+          
+        }else if(value==="2"){
+            setMaxQtt(dress.size2)
+            num = "M"
+         
+        }else {
+            setMaxQtt(dress.size3)
+            num = "L"
+           
+        }
+        setCartData({
+            ...cartData,
+            c_size : num
+        })
+        console.log(num)
+    }
+    const qttChange = (e)=>{
+        setCartData({
+            ...cartData,
+            c_amount : e.target.value,
         })
     }
     return (
@@ -128,16 +188,23 @@ const DetailDress = () => {
                         <li>US 10 / IT 40 / KR 66-77 / size – L <span>재고 :  {dress.size3}</span></li>
                     </ul>
                     <div>
-                        <select id='slt' onChange={ e =>{
-                            e.target.value === "1" ?  setQtt(dress.size1) : e.target.value === "2" ? setQtt(dress.size2) : setQtt(dress.size3)
-                            console.log(qtt)
-                        }}>
+                        {/* <select id='slt' onChange={ e =>{
+                            e.target.value === "1" ?  setMaxQtt(dress.size1) : e.target.value === "2" ? setMaxQtt(dress.size2) : setMaxQtt(dress.size3);
+                            e.target.value === "1" ?  setSize("S") : e.target.value === "2" ? setSize("M") : setSize("L");
+                            setCartData({
+                                ...cartData,
+                                c_size : size
+                            })
+                            
+                            console.log(cartData.c_size)
+                        }}> */}
+                        <select id='slt' onChange={selectOnChange}>
                             <option value="1" className='opQtt'>S</option>
                             <option value="2" className='opQtt'>M</option>
                             <option value="3" className='opQtt'>L</option>
                         </select>
-                        <input id='qttNum' type="number" min="0" max={qtt} />
-                        <button>ADD TO CART</button>
+                        <input id='qttNum' type="number" min="0" max={maxQtt} onChange={qttChange} />
+                        <button onClick={addToCart}>ADD TO CART</button>
                         <div>
                             <button id='editBtn'>EDIT</button>
                             <button id='deleteBtn' onClick={onDelete}>DELETE</button>
