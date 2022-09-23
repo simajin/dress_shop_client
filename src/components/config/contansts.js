9d7f8470ebb92b877c23f6c260f22b4b9a5e4769
yejin <cathkid00@gmail.com>
2022-09-23 15:58:42 +0900

    배포!!!

--- src/components/config/contansts.js
+++ src/components/config/contansts.js
@@ -1,2 +1,6 @@
 export const API_URL = "https://dress-shop-server.herokuapp.com";
 // export const API_URL = "http://localhost:8000";
+
+// export const API_URL = process.env.NODE_ENV === 'production' 
+// ? "https://dress-shop-server.herokuapp.com"
+// :"http://localhost:8000";
